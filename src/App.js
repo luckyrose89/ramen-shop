7c9c3da096ebbc5aa6ca87f45494ad14a5b1171a
avidrutham <divyamathur89@gmail.com>
2020-07-22 17:32:48 +0530

    add homepage to app.js and react-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
-    <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
-      <div className="ml-6 pt-1">
-        <h1 className="text-2xl text-blue-700 leading-tight">
-          Tailwind and Create React App
-        </h1>
-        <p className="text-base text-gray-700 leading-normal">
-          Building apps together
-        </p>
-      </div>
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+
+const HomePage = () => <div className="homepage"></div>;
+
+export default HomePage;
