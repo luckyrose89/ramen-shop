a5a769c67e035cb1aa65a355e4a0ffc0e663565e
avidrutham <divyamathur89@gmail.com>
2020-08-21 14:29:10 +0530

    clear homepage component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 import "./homepage.styles.scss";
 
-const HomePage = () => <div className="homepage"></div>;
+const HomePage = () => <div className="homepage">Ramen Shop</div>;
 
 export default HomePage;
