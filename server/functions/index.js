41e235c3b681ba27b00e77e76381d23cc063fe9d
avidrutham <divyamathur89@gmail.com>
2020-08-22 22:14:20 +0530

    redefine api get route

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
-const app = require("express");
+const app = require("express")();
 
 const { getMenu } = require("./API/menu");
 
-app.get("/menu", getMenu);
+app.get("/", getMenu);
 exports.api = functions.https.onRequest(app);
