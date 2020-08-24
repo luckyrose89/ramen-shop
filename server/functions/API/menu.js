04f40488053929efb77be89e66e052aef43b79e1
avidrutham <divyamathur89@gmail.com>
2020-08-24 20:42:09 +0530

    create get menu items and add menu category function to api

--- server/functions/API/menu.js
+++ server/functions/API/menu.js
@@ -2,8 +2,37 @@
 
 const { db } = require("../utils/admin");
 
+// get all menu items
 exports.getMenu = async (request, response) => {
-  const snapShot = await db.collection("menuItems").get();
-  const menuItems = snapShot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-  response.json({ menuItems });
+  try {
+    const snapShot = await db.collection("menuItems").get();
+    const menuItems = snapShot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return response.json({ menuItems });
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({ error });
+  }
+};
+
+// post request to create menu category
+exports.postCategory = async (request, response) => {
+  try {
+    if (request.body.category.trim() === "") {
+      return response.status(400).json({ category: "must not be empty" });
+    }
+
+    const newCategory = {
+      category: request.body.category,
+      items: [],
+    };
+
+    const categoryCreated = await db.collection("menuItems").add(newCategory);
+    return response.json({ categoryCreated });
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({ error: "something went wrong!" });
+  }
 };
