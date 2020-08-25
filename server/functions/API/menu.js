8ed80ef87144851c41010979ed6667cd962fa69a
avidrutham <divyamathur89@gmail.com>
2020-08-25 22:11:08 +0530

    rewrite db and change request to get menu items

--- server/functions/API/menu.js
+++ server/functions/API/menu.js
@@ -5,7 +5,7 @@ const { db } = require("../utils/admin");
 // get all menu items
 exports.getMenu = async (request, response) => {
   try {
-    const snapShot = await db.collection("menuItems").get();
+    const snapShot = await db.collection("menu-types").get();
     const menuItems = snapShot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
@@ -17,22 +17,31 @@ exports.getMenu = async (request, response) => {
   }
 };
 
-// post request to create menu category
-exports.postCategory = async (request, response) => {
-  try {
-    if (request.body.category.trim() === "") {
-      return response.status(400).json({ category: "must not be empty" });
-    }
+// // post request to create menu category
+// exports.postCategory = async (request, response) => {
+//   try {
+//     if (request.body.category.trim() === "") {
+//       return response.status(400).json({ category: "must not be empty" });
+//     }
 
-    const newCategory = {
-      category: request.body.category,
-      items: [],
-    };
+//     const newCategory = {
+//       category: request.body.category,
+//       items: [],
+//     };
 
-    const categoryCreated = await db.collection("menuItems").add(newCategory);
-    return response.json({ categoryCreated });
-  } catch (error) {
-    console.log(error);
-    return response.status(500).json({ error: "something went wrong!" });
-  }
-};
+//     const categoryCreated = await db.collection("menuItems").add(newCategory);
+//     return response.json({ categoryCreated });
+//   } catch (error) {
+//     console.log(error);
+//     return response.status(500).json({ error: "something went wrong!" });
+//   }
+// };
+
+// // post to create and items to category
+// exports.postItems = async(request, response) => {
+//   try {
+
+//   }catch(error) {
+
+//   }
+// }
