cd8e7513a148c44ec73995d9b601efcd5b1faa4e
avidrutham <divyamathur89@gmail.com>
2020-09-08 15:56:31 +0530

    add axios, create menuItem and category component files. Get Menu Items with axios.get

--- src/pages/menu/menupage.component.jsx
+++ src/pages/menu/menupage.component.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import axios from "axios";
 
-const MenuPage = () => {
-  return <div>Menu Page</div>;
-};
+class MenuPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menuItems: [],
+    };
+  }
+
+  componentDidMount = async () => {
+    try {
+      const menuData = await axios.get(
+        "http://localhost:5000/ramen-shop/us-central1/api"
+      );
+      this.setState({
+        menuItems: menuData.data.menu,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  filterItemsByCategory = (category) => {};
+
+  render() {
+    const items = this.state.menuItems.map((item) => {
+      return <div key={item.id}>{item.name}</div>;
+    });
+    return <div>{items}</div>;
+  }
+}
 
 export default MenuPage;
