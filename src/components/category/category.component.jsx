cbd5f7339a36037897f05b63daf15d3cb68ea518
avidrutham <divyamathur89@gmail.com>
2020-09-11 15:08:25 +0530

    complete basic grid for menu-items page

--- src/components/category/category.component.jsx
+++ src/components/category/category.component.jsx
@@ -4,11 +4,11 @@ import MenuItem from "../menu-item/menuItem.component";
 const Category = (props) => {
   console.log(props);
   return (
-    <div>
-      <h2>{props.category.category}</h2>
-      <div>
+    <div className="mb-5">
+      <h2 className="mb-8 capitalize">{props.category.category}</h2>
+      <div className="flex flex-wrap">
         {props.category.items.map((item) => {
-          return <MenuItem {...item} />;
+          return <MenuItem key={item.id} {...item} />;
         })}
       </div>
     </div>
--- src/components/menu-item/menuItem.component.jsx
+++ src/components/menu-item/menuItem.component.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import "./menuItem.styles.scss";
+
 const MenuItem = ({ name, price, description, imageURL }) => {
-  return <div>{name}</div>;
+  return (
+    <div className="menugrid border-2 border-gray-600 mr-4 mb-4 rounded-md overflow-hidden cursor-pointer">
+      <div
+        className="w-full h-48 bg-cover bg-no-repeat bg-top"
+        style={{ backgroundImage: `url(${imageURL})` }}
+      ></div>
+      <div className="px-6 py-4 text-center">
+        <h4>{name}</h4>
+        <p>{description}</p>
+        <div>${price.toFixed(2)}</div>
+      </div>
+    </div>
+  );
 };
 
 export default MenuItem;
