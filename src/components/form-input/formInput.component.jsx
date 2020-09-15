42df6d213b46978bba5c3defb6cf338279597c97
avidrutham <divyamathur89@gmail.com>
2020-09-15 13:36:44 +0530

    create login and sign up page with basic styles

--- src/components/form-input/formInput.component.jsx
+++ src/components/form-input/formInput.component.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const FormInput = ({ handleChange, label, ...otherProps }) => {
   return (
-    <div>
-      <input type="text" onChange={handleChange} {...otherProps} />
+    <div className="mb-4">
+      <label className="block text-gray-700 text-sm sm:text-base font-bold mb-2">
+        {label}
+      </label>
+      <input
+        type="text"
+        onChange={handleChange}
+        {...otherProps}
+        className="shadow appearance-none border py-2 px-3 w-full"
+      />
     </div>
   );
 };
--- src/pages/loginSignup/signup.component.jsx
+++ src/pages/loginSignup/signup.component.jsx
@@ -1,7 +1,72 @@
 import React from "react";
+import FormInput from "../../components/form-input/formInput.component";
 
-const SignupPage = () => {
-  return <div>Sign up Page</div>;
-};
+class SignupPage extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  handleChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="max-w-sm mx-auto mt-20 px-5">
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="username"
+            type="text"
+            label="Username"
+            value={this.state.username}
+            handleChange={this.handleChange}
+            required
+          />
+          <FormInput
+            name="email"
+            type="email"
+            label="Email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            required
+          />
+          <FormInput
+            name="password"
+            type="password"
+            label="Password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            required
+          />
+          <FormInput
+            name="confirmPassword"
+            type="password"
+            label="Confirm Password"
+            value={this.state.confirmPassword}
+            handleChange={this.handleChange}
+            required
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 mt-4 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default SignupPage;
