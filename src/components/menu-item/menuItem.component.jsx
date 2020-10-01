15d428b85ba01d4b59846e74e4fafae91d7c6b29
avidrutham <divyamathur89@gmail.com>
2020-10-01 22:18:30 +0530

    add span for item count in cart icon and create cart dropdown component

--- src/components/menu-item/menuItem.component.jsx
+++ src/components/menu-item/menuItem.component.jsx
@@ -7,7 +7,7 @@ const MenuItem = ({ name, price, description, imageURL }) => {
         <h4 className="pb-2">{name}</h4>
         <div className="text-xs md:text-base pb-2">${price.toFixed(2)}</div>
         <p className="text-xs md:text-base text-gray-700 pb-4">{description}</p>
-        <button className="hidden sm:block px-3 py-2 bg-green-600 hover:bg-green-800 text-white rounded-lg text-sm">
+        <button className="hidden sm:block px-3 py-2 bg-green-600 hover:bg-green-800 text-white rounded-lg text-sm focus:outline-none border-2 focus:border-green-900">
           Add
         </button>
       </div>
@@ -15,7 +15,7 @@ const MenuItem = ({ name, price, description, imageURL }) => {
         className="relative w-24 h-24 sm:w-48 sm:h-auto bg-cover self-center sm:self-auto"
         style={{ backgroundImage: `url(${imageURL})` }}
       >
-        <button className="absolute sm:hidden px-3 py-2 bg-green-600 hover:bg-green-800 text-white text-sm">
+        <button className="absolute sm:hidden px-3 py-2 bg-green-600 hover:bg-green-800 text-white text-sm focus:outline-none">
           Add
         </button>
       </div>
