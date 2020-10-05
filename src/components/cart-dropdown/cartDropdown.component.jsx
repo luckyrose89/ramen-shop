5e69f68457f7478e6607afde80763956cf6989a9
avidrutham <divyamathur89@gmail.com>
2020-10-05 16:13:10 +0530

    create and style cartItem component and map through cartItems in cartDropdown component

--- src/components/cart-dropdown/cartDropdown.component.jsx
+++ src/components/cart-dropdown/cartDropdown.component.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 import CartItem from "../cart-item/cartItem.component";
 
 import "./cartDropdown.styles.scss";
 
-const CartDropdown = ({ history, dispatch }) => {
+const CartDropdown = ({ history, dispatch, cartItems }) => {
   return (
-    <div className="absolute cart-dropdown w-56 h-64 z-10 px-4 shadow-lg flex flex-col items-center">
-      <CartItem />
+    <div className="absolute cart-dropdown w-64 z-10 px-4 shadow-lg flex flex-col items-center">
+      <div className="flex flex-col overflow-y-scroll">
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span>Your Cart is Empty!</span>
+        )}
+      </div>
       <button
         onClick={() => {
           history.push("/checkout");
+          dispatch(toggleCartHidden());
         }}
-        className=" w-40 h-12 p-2 bg-gray-700 text-white text-sm uppercase hover:bg-gray-500"
+        className=" w-40 h-12 p-2 mt-auto mb-4 bg-gray-700 text-white text-sm uppercase hover:bg-gray-500"
       >
         Go To Checkout
       </button>
@@ -22,4 +34,8 @@ const CartDropdown = ({ history, dispatch }) => {
   );
 };
 
-export default withRouter(CartDropdown);
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+});
+
+export default withRouter(connect(mapStateToProps)(CartDropdown));
--- src/components/cart-item/cartItem.component.jsx
+++ src/components/cart-item/cartItem.component.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const CartItem = () => {
-  return <div></div>;
+const CartItem = ({ item: { name, price, quantity, imageURL } }) => {
+  return (
+    <div className="w-full flex h-20 mb-4">
+      <img className="w-1/3" src={imageURL} alt="item preview" />
+      <div className="w-2/3 flex flex-col items-start px-4 hover:underline">
+        <span className="text-sm">{name}</span>
+        <span className="text-sm">
+          {quantity} X {price}
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
