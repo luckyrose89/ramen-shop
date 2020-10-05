c86532822eab57f64b84b2afff4166681020f92d
avidrutham <divyamathur89@gmail.com>
2020-10-05 22:13:27 +0530

    create cart Checkout component and connect checkout page to cart state

--- src/components/checkout-item/checkoutItem.component.jsx
+++ src/components/checkout-item/checkoutItem.component.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
 
-const CheckoutItem = () => {
-  return <div></div>;
+import {
+  addItemToCart,
+  removeItemFromCart,
+  clearItemFromCart,
+} from "../../redux/cart/cart.actions";
+
+const CheckoutItem = ({ cartItem, addItem, removeItem, clearItem }) => {
+  const { name, price, imageURL, quantity } = cartItem;
+  return (
+    <div>
+      <div>
+        <img src={imageURL} alt="item preview" />
+      </div>
+      <span>{name}</span>
+      <span>
+        <button> &#10094;</button>
+        <span>{quantity}</span>
+        <button> &#10095;</button>
+      </span>
+      <span>{price}</span>
+      <button> &#10005;</button>
+    </div>
+  );
 };
 
-export default connect()(CheckoutItem);
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItemToCart(item)),
+  removeItem: (item) => dispatch(removeItemFromCart(item)),
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../redux/cart/cart.selectors";
 
-const CheckoutPage = () => {
-  return <div>Checkout page</div>;
+import CheckoutItem from "../../components/checkout-item/checkoutItem.component";
+
+const CheckoutPage = ({ cartItems, total }) => {
+  return (
+    <div className="px-5 mt-10">
+      <h1 className="text-center sm:text-2xl uppercase text-gray-800 my-12">
+        Checkout
+      </h1>
+      <div className="hidden sm:flex">
+        <div>
+          <span>Product</span>
+        </div>
+        <div>
+          <span>Description</span>
+        </div>
+        <div>
+          <span>Quantity</span>
+        </div>
+        <div>
+          <span>Price</span>
+        </div>
+        <div>
+          <span>Remove</span>
+        </div>
+      </div>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+    </div>
+  );
 };
 
-export default CheckoutPage;
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal,
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
