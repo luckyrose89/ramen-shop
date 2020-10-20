ab9f0639b6ea1d7382a232ae28578eec67146ce7
avidrutham <divyamathur89@gmail.com>
2020-10-20 15:55:10 +0530

    add form labels to user info edit component

--- src/components/userInfo-edit-form/userInfoEditForm.component.jsx
+++ src/components/userInfo-edit-form/userInfoEditForm.component.jsx
@@ -40,54 +40,77 @@ class UserInfoEditForm extends React.Component {
     return (
       <div className="relative max-w-xl mx-auto shadow-lg h-auto w-full py-8 px-8 text-sm sm:text-base">
         <form className="mt-2" onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            className="my-2 py-1 px-1 mt-3 w-full border-solid border-b border-gray-500"
-            value={this.state.username}
-            onChange={this.handleChange}
-            name="username"
-            aria-label="Username"
-            autoFocus
-            required
-          />
-          <div className="sm:flex">
+          <div className="my-2 flex flex-col">
+            <label className="px-1 text-sm text-gray-900 font-bold">
+              Username
+            </label>
             <input
               type="text"
-              className="my-2 py-1 px-1 w-full sm:w-1/2 border-solid border-b border-gray-500 mr-2"
-              value={this.state.firstname}
+              className="py-1 px-1 w-full border-solid border-b border-gray-500"
+              value={this.state.username}
               onChange={this.handleChange}
-              name="firstname"
-              aria-label="First Name"
-              required
-            />
-            <input
-              type="text"
-              className="my-2 py-1 px-1 w-full sm:w-1/2 border-solid border-b border-gray-500"
-              value={this.state.lastname}
-              onChange={this.handleChange}
-              name="lastname"
-              aria-label="Last Name"
+              name="username"
               required
             />
           </div>
-          <input
-            type="text"
-            className="my-2 py-1 px-1 w-full border-solid border-b border-gray-500"
-            value={this.state.email}
-            onChange={this.handleChange}
-            name="email"
-            aria-label="Email"
-            required
-          />
-          <input
-            type="text"
-            className="my-2 py-1 px-1 w-full border-solid border-b border-gray-500"
-            value={this.state.address}
-            onChange={this.handleChange}
-            name="address"
-            aria-label="Address"
-            required
-          />
+          <div className="sm:flex">
+            <div className="my-2 flex flex-col w-full sm:w-1/2">
+              <label className="px-1 text-sm text-gray-900 font-bold">
+                First Name
+              </label>
+              <input
+                type="text"
+                className="py-1 px-1 border-solid border-b border-gray-500 mr-2"
+                value={this.state.firstname}
+                onChange={this.handleChange}
+                name="firstname"
+                aria-label="First Name"
+                required
+              />
+            </div>
+            <div className="my-2 flex flex-col w-full sm:w-1/2">
+              <label className="px-1 text-sm text-gray-900 font-bold">
+                Last Name
+              </label>
+              <input
+                type="text"
+                className="py-1 px-1 border-solid border-b border-gray-500"
+                value={this.state.lastname}
+                onChange={this.handleChange}
+                name="lastname"
+                aria-label="Last Name"
+                required
+              />
+            </div>
+          </div>
+          <div className="my-2 flex flex-col">
+            <label className="px-1 text-sm text-gray-900 font-bold">
+              Email
+            </label>
+            <input
+              type="text"
+              className="py-1 px-1 w-full border-solid border-b border-gray-500"
+              value={this.state.email}
+              onChange={this.handleChange}
+              name="email"
+              aria-label="Email"
+              required
+            />
+          </div>
+          <div className="my-2 flex flex-col">
+            <label className="px-1 text-sm text-gray-900 font-bold">
+              Address
+            </label>
+            <input
+              type="text"
+              className="py-1 px-1 w-full border-solid border-b border-gray-500"
+              value={this.state.address}
+              onChange={this.handleChange}
+              name="address"
+              aria-label="Address"
+              required
+            />
+          </div>
           <button
             type="submit"
             className="absolute top-0 right-0 mt-5 mr-5 py-1 px-2 text-green-800 font-bold"
