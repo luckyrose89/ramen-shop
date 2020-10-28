91ca590f3450143ed648a261f1d84419a0bb4229
avidrutham <divyamathur89@gmail.com>
2020-10-28 21:53:28 +0530

    create menu Items actions and add thunk to dispatch actions after api call

---
+++ src/redux/menu/menu.actions.js
@@ -0,0 +1,29 @@
+import MenuActionTypes from "./menu.types";
+import axios from "axios";
+
+export const fetchMenuItemsStart = () => ({
+  type: MenuActionTypes.FETCH_MENU_START,
+});
+
+export const fetchMenuItemsSuccess = (items) => ({
+  type: MenuActionTypes.FETCH_MENU_SUCCESS,
+  payload: items,
+});
+
+export const fetchMenuItemsFailure = (errorMessage) => ({
+  type: MenuActionTypes.FETCH_MENU_FAILURE,
+  payload: errorMessage,
+});
+
+export const getMenuItems = async (dispatch) => {
+  try {
+    dispatch(fetchMenuItemsStart());
+    const itemsResult = await axios.get(
+      "https://us-central1-ramen-shop.cloudfunctions.net/api"
+    );
+    dispatch(fetchMenuItemsSuccess(itemsResult.data.menu));
+  } catch (error) {
+    console.log(error);
+    dispatch(fetchMenuItemsFailure(error.message));
+  }
+};
