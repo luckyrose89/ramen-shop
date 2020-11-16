87c5d986a4486bda543437581a97c994254894a6
avidrutham <divyamathur89@gmail.com>
2020-11-16 10:08:10 +0530

    edit admin function in server to fetch user by email before adding custom claims

--- server/functions/API/admin.js
+++ server/functions/API/admin.js
@@ -3,10 +3,13 @@
 const admin = require("firebase-admin");
 
 exports.makeAdminUser = async (request, response) => {
-  const { userId } = request.body;
+  const { userEmail } = request.body;
   try {
-    await admin.auth().setCustomUserClaims(userId, { admin: true });
-    return response.json({ message: "Successfully set as admin!" });
+    const user = await admin.auth().getUserByEmail(userEmail);
+    if (user.emailVerified) {
+      await admin.auth().setCustomUserClaims(user.uid, { admin: true });
+      return response.json({ message: "Successfully set as admin!" });
+    }
   } catch (error) {
     console.log(error);
     return response.status(500).json({ error });
