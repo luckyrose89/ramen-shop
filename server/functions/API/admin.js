596b146cb82f9db5d56a94a72ca77500fd98ea0e
avidrutham <divyamathur89@gmail.com>
2020-11-16 10:58:38 +0530

    edit admin function to set custom claims on users

--- server/functions/API/admin.js
+++ server/functions/API/admin.js
@@ -5,11 +5,10 @@ const admin = require("firebase-admin");
 exports.makeAdminUser = async (request, response) => {
   const { userEmail } = request.body;
   try {
-    const user = await admin.auth().getUserByEmail(userEmail);
-    if (user.emailVerified) {
-      await admin.auth().setCustomUserClaims(user.uid, { admin: true });
-      return response.json({ message: "Successfully set as admin!" });
-    }
+    let user = await admin.auth().getUserByEmail(userEmail);
+
+    await admin.auth().setCustomUserClaims(user.uid, { admin: true });
+    return response.json({ message: "Successfully set as admin!" });
   } catch (error) {
     console.log(error);
     return response.status(500).json({ error });
